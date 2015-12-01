6f552e3a7bb841b582242809d013727cacdb39e6
misxenia <xeniamisc@gmail.com>
2015-12-01 18:36:26 +0000

    r file for multi_reg2

--- R/multi_regr.R
+++ R/multi_regr.R
@@ -1,3 +1,14 @@
+##Bootstrap function that performs regression of y on x and computes the stadard deviation of
+# the regression coefficients
+
+#' Bootstrap
+#' @param data the original sample, as a vector
+#' @param  data is a vector
+#' @param B the number of bootstrap replications
+#' @param n the number of resamples to generate
+#' @return vector with the standard deviation of the regression coeffiecients
+#' @examples
+#' bootstrap(,,,,,)
 
  bootstrap <- function (x, y, B, n) {
 
