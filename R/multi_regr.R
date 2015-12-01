e97edd63ee93d22fc0a7d8ef1fdbb81f7146be1f
misxenia <xeniamisc@gmail.com>
2015-12-01 12:17:12 +0000

    multi reg with pointers

--- R/multi_regr.R
+++ R/multi_regr.R
@@ -1,1 +1,10 @@
-multi_regr.R
+
+ bootstrap <- function (x, y, B, n) {
+
+  d <- ncol( x )
+  b <- nrow( x )
+  ans <- .C('bootstrap', as.double( x ), as.double( y ), as.double (numeric ( d )) , as.integer( b ), as.integer (B), as.integer( n ), as.integer( d ) )
+  return (ans[[3]])
+
+}
+
