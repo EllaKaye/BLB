cb76818bdb7a92efe0858f26697c0c60de7ee737
EllaKaye <ella.kaye@gmail.com>
2015-12-02 23:16:06 +0000

    with multidim and man page

--- R/bootstrap_multidim.R
+++ R/bootstrap_multidim.R
@@ -1,19 +1,24 @@
-##Bootstrap function that performs regression of y on x and computes the stadard deviation of
-# the regression coefficients
-
-#' bootstrap_multidim
-#' @param data the original sample, as a vector
-#' @param  data is a vector
-#' @param B the number of bootstrap replications
-#' @param n the number of resamples to generate
+#' Bootstraps for a regression coefficients
+#' 
+#' Bootstrap functions that performs regression of y on X and computes the standard deviation of the regression coefficients
+#' 
+#' The functions described here are designed as helper functions for the Bag of Little Bootstraps. When used in the BLB, either of these functions can be called on each of the s subsamples, with the matrix X and vector y being a subsample of the full data matrix and response vector.
+#' 
+#' \code{bootstrap_multidim} runs in serial over the r subsamples. 
+#' @param X The data as a matrix
+#' @param y The response variable for the regression
+#' @param r the number of bootstrap resamples
+#' @param n the number of data points in each resample (which is also the number of data points in the full data set)
 #' @return vector with the standard deviation of the regression coeffiecients
+#' @examples 
+#' X <- matrix(rnorm(10), 5, 2)
+#' y <- rnorm(5)
+#' bootstrap_multidim(X, y, 1000, 30)
+bootstrap_multidim <- function (X, y, r, n) {
 
-
-bootstrap_multidim <- function (x, y, B, n) {
-
-  d <- ncol( x )
-  b <- nrow( x )
-  ans <- .C("bootstrap_multidim", as.double( x ), as.double( y ), result = as.double (numeric ( d )) , as.integer( b ), as.integer (B), as.integer( n ), as.integer( d ) )
+  d <- ncol( X )
+  b <- nrow( X )
+  ans <- .C("bootstrap_multidim", as.double( X ), as.double( y ), result = as.double (numeric ( d )) , as.integer( b ), as.integer (r), as.integer( n ), as.integer( d ) )
   return(ans$result)
-
 }
+
